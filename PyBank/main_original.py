392ab6448994d58d045a201f5bf78e9c711bcc80
Kerigo Ndirangu <62352071+kgndirangu@users.noreply.github.com>
2024-01-18 18:26:26 -0500

    second attempt

--- PyBank/main_original.py
+++ PyBank/main_original.py
@@ -10,6 +10,8 @@ with open(bank, 'r') as csvfile:
 
     # Scdplit the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
+    #code doesn't run if header is not skipped, however 2 removes the header and first row of data
+    # leading to underestimate in total amount  
     header = next(csvreader)
     
     #remove first row from amount change calculation
@@ -22,12 +24,6 @@ with open(bank, 'r') as csvfile:
     amount_list = []
     change_list = []
 
-    max_increase = 0
-    max_decrease = 10000000000
-    max_inc_month = ""
-    max_dec_month = ""
-
-
     #start at month 1 since we removed first row
     total_month = 1
     amount_list.append(int(previous_row[1]))   
@@ -38,36 +34,39 @@ with open(bank, 'r') as csvfile:
         month_list.append(row[0])
         #total amount calculation, append to store in amount list; convert to integer 
         amount_list.append(int(row[1])) 
-
         #The changes in profit/losses over the entire period, and then the average of those changes 
         change = int(row[1]) - previous_value
-        if max_increase < change:
-            max_increase= change
-            max_inc_month = row[0]
-        if max_decrease > change:
-            max_decrease= change
-            max_dec_month = row[0]
         #after calculation reset previous value
         previous_value = int(row[1])
         #store change in list
         change_list.append(change)
         
-
-       
-   
+        
+        
+        
+#first row = 1088983 is missing from total_amount, please advise    
 total_amount = sum(amount_list)
 avg_change = round(sum(change_list)/len(change_list), 2)
+greatest = max(change_list)
+least = min(change_list)
+#greatest increase and month
+month_g = change_list.index(max(change_list))
+#index 78 based on previous line output
+month_g2 = month_list[78]
+#greatest decrease and month
+month_l = change_list.index(min(change_list))
+#index 48 based on previous line output
+month_l2 = month_list[48]
 
 
 
 output = (
         f"Financial Analysis\n"
-        f"----------------------------\n"
         f"Total Months: {total_month}\n"
         f"Total: ${total_amount}\n"
         f"Average Change: ${avg_change}\n"
-        f"Greatest Increase in Profits: {max_inc_month} (${max_increase})\n"
-        f"Greatest Decrease in Profits: {max_dec_month} (${max_decrease})"
+        f"Greatest Increase in Profits: {month_g2} (${greatest})\n"
+        f"Greatest Decrease in Profits: {month_l2} (${least})"
     )
 
 print(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,50 +2,59 @@ import os
 import csv
 
 #path to collect data from Resources folder
-election_data = os.path.join('..', 'Resources', 'election_data.csv') 
+election_data = os.path.join('Resources', 'election_data.csv') 
 
-
+candidates_list = []
+candidates_votes = {}
 # Read in the CSV file
 with open(election_data, 'r') as csvfile:
        
 
     # Scdplit the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
     header = next(csvreader)
     
     total_votes = 0
-    vote_c = 0
-    vote_d = 0
-    vote_r = 0
-    #plan was to create 2 dictionaries. One with the 3 candidates and percentage votes recieved, 
-    # the other with the 3 candidates and total votes, however was unable to write syntax so I solved the 
-    #challenge in a roundabout way 
-    
+  
     for row in csvreader: 
         
         total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidates_list: 
+            candidates_list.append(candidate_name)
+            candidates_votes[candidate_name] = 0
+        candidates_votes[candidate_name] += 1
         
-        if row[2]== "Charles Casper Stockham":
-            vote_c = vote_c +1
-        elif row[2] == "Diana DeGette": 
-            vote_d = vote_d + 1
-        else:
-            vote_r = vote_r +1
+      
+percentages = {key: round(val / total_votes*100,3) for key,val in candidates_votes.items()}
+winning_candidate = max(candidates_votes, key=candidates_votes.get)
+
+output_file = os.path.join('analysis', 'election_analysis.txt') 
 
 
-perc_c = (vote_c/total_votes)*100 
-perc_d = (vote_d/total_votes)*100 
-perc_r = (vote_r/total_votes)*100 
+with open(output_file, 'w') as txtfile:
+    
 
-output = (
-        f"Election Results\n"
-        f"Total Votes: {total_votes}\n"
-        f"Charles Casper Stockham: {str(perc_c)}%, ({str(vote_c)})\n"
-        f"Diane DeGette: {str(perc_d)}%, ({str(vote_d)})\n"
-        f"Raymon Anthony Doane: {str(perc_r)}%, ({str(vote_r)})"
-            )
+    output1 = (
+            f"Election Results\n"
+            f"-------------------------\n"
+            f"Total Votes: {total_votes}\n"
+            f"-------------------------\n")
+    print(output1)
+    txtfile.write(output1)
 
-print(output)
 
-#unable to print winner because did not successfully manipulate dictionary to find percentage vote each candidate received
+        
+    for k,v in candidates_votes.items():
+        output2 = f"{k}: {percentages[k]}% ({v})\n"
+        print(output2)
+        txtfile.write(output2)
+
+
+
+    output3 = ( f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"-------------------------"
+                )
+    print(output3)
+    txtfile.write(output3)
